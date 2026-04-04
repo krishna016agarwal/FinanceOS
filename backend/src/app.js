14bb94613ce6327487c8051e73c0122ae9ef0707
Krishna Agarwal <krishna016agrawal@gmail.com>
2026-04-04 17:17:49 +0530

    add frontend url to the cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ connectDB();
 app.use(helmet());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', process.env.FRONTEND_URL],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
